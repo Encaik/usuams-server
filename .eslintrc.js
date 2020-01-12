40dcb480021036a6f497bca26f79612e81b09ca3
Encaik <843391622@qq.com>
2020-01-12 10:54:29 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: "eslint-config-egg",
+  rules: {
+    "no-console": 0,
+    "linebreak-style": [0, "windows"]
+  }
+}
---
+++ config/config.default.js
@@ -0,0 +1,42 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1578792997450_5942';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: '123456',
+      // database
+      database: 'usuams',
+    },
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
