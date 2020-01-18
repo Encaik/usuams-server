15d5477748ea87eb340fa7aac08b764703ad19fc
Encaik <843391622@qq.com>
2020-01-18 09:44:44 +0800

    完善事务模块

--- app/service/affair.js
+++ app/service/affair.js
@@ -12,7 +12,12 @@ class AffairService extends Service {
 
   // 按id查询数据
   async show(params) {
-    const result = await this.app.mysql.select('affair_table', { id: params.id });
+    const result = await this.app.mysql.select(
+      'affair_table',
+      {
+        id: params.id,
+      }
+    );
     return result;
   }
 
@@ -22,19 +27,47 @@ class AffairService extends Service {
       'affair_table',
       {
         name: body.name,
-        number: body.number,
+        create_date: body.create_date,
+        start_date: body.start_date,
+        end_date: body.end_date,
+        leader: body.leader,
+        level: body.level,
+        state: body.state,
+        context: body.context,
       }
     );
     return result;
   }
 
   async update(params, body) {
+    const data = {};
+    if (body.name) {
+      data.name = body.name;
+    }
+    if (body.create_date) {
+      data.create_date = body.create_date;
+    }
+    if (body.start_date) {
+      data.start_date = body.start_date;
+    }
+    if (body.end_date) {
+      data.end_date = body.end_date;
+    }
+    if (body.leader) {
+      data.leader = body.leader;
+    }
+    if (body.level) {
+      data.level = body.level;
+    }
+    if (body.state) {
+      data.state = body.state;
+    }
+    if (body.context) {
+      data.context = body.context;
+    }
     const result = await this.app.mysql.update(
       'affair_table',
-      {
-        name: body.name,
-        number: body.number,
-      },
+      data,
       {
         where: {
           id: params.id,
